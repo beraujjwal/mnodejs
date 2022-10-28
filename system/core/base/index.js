3e3a71f47ab57ddb1a0f01fd62ca3a6ce5bdda17
Ujjwal Bera <87612865+beraujjwal@users.noreply.github.com>
2022-10-28 15:43:29 +0530

    Update index.js
    base file add error handlers

--- system/core/base/index.js
+++ system/core/base/index.js
@@ -3,6 +3,19 @@ const autoBind = require('auto-bind');
 const db = require('../model');
 require('dotenv').config();
 
+const { 
+  BadRequestError, 
+  UnauthorizedError, 
+  PaymentRequiredError, 
+  ForbiddenError, 
+  NotFoundError, 
+  MethodNotAllowedError, 
+  InternalServerError, 
+  BadGatewayError, 
+  ServiceUnavailableError, 
+  CommonError 
+} = require('../errorHandler/baseError')
+
 class index {
   /**
    * Base Controller Layer
@@ -14,6 +27,46 @@ class index {
     this.transaction = db.trans;
     this.env = process.env;
     autoBind(this);
+  }
+
+  async BadRequestError(message, code = 400) {
+    throw new BadRequestError(message, code);
+  }
+  
+  async UnauthorizedError(message, code = 401) {
+    throw new UnauthorizedError(message, code);
+  }
+  
+  async PaymentRequiredError(message, code = 402) {
+    throw new PaymentRequiredError(message, code);
+  }
+  
+  async ForbiddenError(message, code = 403) {
+    throw new ForbiddenError(message, code);
+  }
+
+  async NotFoundError(message, code = 404) {
+    throw new NotFoundError(message, code);
+  }
+  
+  async MethodNotAllowedError(message, code = 405) {
+    throw new MethodNotAllowedError(message, code);
+  }
+  
+  async InternalServerError(message, code = 500) {
+    throw new InternalServerError(message, code);
+  }
+  
+  async BadGatewayError(message, code = 502) {
+    throw new BadGatewayError(message, code);
+  }
+
+  async ServiceUnavailableError(message, code = 503) {
+    throw new ServiceUnavailableError(message, code);
+  }
+  
+  async CommonError(message, code) {
+    throw new CommonError(message, code);
   }
 }
 
